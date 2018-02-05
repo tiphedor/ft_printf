202b5f402f0543997d9f43ec3fbde049476b6d24
Martin STEFFEN <msteffen@student.42.fr>
2018-02-05 15:22:12 +0100

    Synced libft

--- libft/libft.h
+++ libft/libft.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: msteffen <msteffen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/06 16:00:07 by msteffen          #+#    #+#             */
-/*   Updated: 2017/12/12 14:02:08 by msteffen         ###   ########.fr       */
+/*   Created: 2018/01/17 18:28:37 by msteffen          #+#    #+#             */
+/*   Updated: 2018/02/05 15:17:25 by msteffen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 # define FT_LIBFT 1
 # define FT_VERSION 1
+# define BUFF_SIZE 60
 
 typedef struct	s_list
 {
@@ -56,6 +57,7 @@ void			ft_memdel(void **ap);
 int				ft_atoi(const char *str);
 char			*ft_itoa(int n);
 
+char			*ft_strreplace(char *haystack, char *needle, char *new_needle);
 char			*ft_strdup(const char *s1);
 char			*ft_strndup(const char *s1, size_t n);
 char			*ft_strnew(size_t size);
@@ -117,5 +119,7 @@ int				ft_int_min(int *arr, unsigned int len);
 int				ft_int_max(int *arr, unsigned int len);
 void			ft_int_sort(int *arr, unsigned int len);
 float			ft_int_average(int *arr, unsigned int len);
+
+int				ft_gnl(const int fd, char **line);
 
 #endif
